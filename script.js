4b6e7b46b249e3baa56186834277aac5915ac3e7
Mr.Igor <iv_mirsoft@ukr.net>
2026-01-19 19:06:11 +0200

    fixed select algo/ add modes for future drag

--- script.js
+++ script.js
@@ -6,11 +6,41 @@ const lineArea = document.querySelector('#line')
 
 const markCanvas = document.querySelector('#marquee')
 
-let isCtrlPressed = false
+let isClickMode = true
+
+let mouseMode = "none"
 
 let textItemData = []
 
-let newX = 0, newY = 0, startX = -1, startY = -1;
+let startX = -1, startY = -1;
+
+const setTextItemData = () => {
+	textItemData = []
+
+	const highlightedItems = document.querySelectorAll('.mark-span')
+
+	highlightedItems.forEach((item) => {
+		const rect = item.getBoundingClientRect();
+
+		textItemData.push({
+			id: item.id,
+			top: rect.top,
+			left: rect.left,
+			right: rect.right,
+			bottom: rect.bottom,
+			isSelected: false,
+			isMarked: false
+		});
+
+	})
+}
+
+const getWidthHeightSelect = (xPos, yPos) => {
+	const width = Math.abs(xPos - startX)
+	const height = Math.abs(yPos - startY)
+
+	return { width, height }
+}
 
 const getHtmlForText = (text) => {
 	const splitted = text.split("")
@@ -27,87 +57,114 @@ const getHtmlForText = (text) => {
 const submitFn = (e) => {
 	e.preventDefault();
 
-	textItemData = []
-
 	const textValuesHtml = getHtmlForText(inputTxt.value.trim())
 	lineArea.innerHTML = textValuesHtml
 
 	submitForm.reset();
 
-	highlightedItems = document.querySelectorAll('.mark-span')
-
-	highlightedItems.forEach((item) => {
-		const rect = item.getBoundingClientRect();
-
-		textItemData.push({
-			id: item.id,
-			top: rect.top,
-			left: rect.left,
-			right: rect.right,
-			bottom: rect.bottom,
-			isSelected: false,
-			isMarked: false
-		});
-
-	})
-
 };
 
-const handleLineClick = (rect) => {
+const handleLineSelect = (rect) => {
 
-	console.log("click", textItemData)
+	console.log("select", textItemData)
 	if (!textItemData.length) return
 
 	//const itemValue = e.target.closest('span')
 
 	//if (!itemValue) {
-	itemList = textItemData.filter((item) => {
+	const itemList = textItemData.filter((item) => {
 		return item.left < rect.right && item.right > rect.left && item.top < rect.bottom && item.bottom > rect.top
 	})
 	//}
-	console.log("click", itemList, rect)
+	console.log("select", itemList, rect)
 	if (!itemList.length) return
 
 	for (const element of itemList) {
-		spanElement = document.querySelector(`#${element.id}`)
-		spanElement.classList.toggle("is-selected")
+		const spanElement = document.querySelector(`#${element.id}`)
+		spanElement.classList.add("is-selected")
 		element.isMarked = spanElement.classList.contains("is-selected")
 	}
 	console.log('click', itemList, rect)
 }
 
+const handleLineClick = (e) => {
+
+	console.log("click", textItemData)
+	if (!textItemData.length) return
+
+	const itemValue = e.target.closest('span')
+
+	if (!itemValue) return
+
+	const itemList = textItemData.filter((item) => {
+		return item.id === itemValue.id
+	})
+
+	if (itemList.length !== 1) return
+
+	itemValue.classList.toggle("is-selected")
+	itemList[0].isMarked = itemValue.classList.contains("is-selected")
+
+	console.log('click', itemList)
+}
+
 const handleMouseDown = (e) => {
-	markCanvas.classList.add('is-drawing')
-	startX = e.clientX
-	startY = e.clientY
+	if (e.ctrlKey) {
+		mouseMode = 'selection'
+
+		setTextItemData()
+
+		isClickMode = true
+
+		startX = e.clientX
+		startY = e.clientY
+
+		markCanvas.classList.add('is-drawing')
+	} else {
+		const targetSpan = e.target.closest('span.mark-span')
+
+		if (!targetSpan || !targetSpan.classList.contains("is-selected")) return
+
+		mouseMode = 'dragOne'
+	}
 }
 
 const handleMouseUp = (e) => {
-	markCanvas.classList.remove('is-drawing')
 
-	const rect = markCanvas.getBoundingClientRect();
+	if (startX >= 0 && startY >= 0) {
+		markCanvas.classList.remove('is-drawing')
 
-	startX = -1
-	startY = -1
+		const rect = markCanvas.getBoundingClientRect();
 
-	markCanvas.style.top = "0px"
-	markCanvas.style.left = "0px"
-	markCanvas.style.width = "0px"
-	markCanvas.style.height = "0px"
+		const { width, height } = getWidthHeightSelect(e.clientX, e.clientY)
 
-	console.log("move", e.ctrlKey, textItemData)
-	if (e.ctrlKey) handleLineClick(rect)
+		isClickMode = width <= 8 && height <= 8
 
+		startX = -1
+		startY = -1
+
+		markCanvas.style.top = "0px"
+		markCanvas.style.left = "0px"
+		markCanvas.style.width = "0px"
+		markCanvas.style.height = "0px"
+
+		console.log("move", e.ctrlKey, textItemData)
+
+		if (e.ctrlKey && mouseMode === 'selection') {
+			if (!isClickMode) handleLineSelect(rect)
+			else handleLineClick(e)
+		}
+	}
 }
 
 const handleMouseMove = (e) => {
+
 	if (startX < 0 && startY < 0) return
 
 	const top = Math.min(e.clientY, startY)
 	const left = Math.min(e.clientX, startX)
 
-	const width = Math.abs(e.clientX - startX)
-	const height = Math.abs(e.clientY - startY)
+	const { width, height } = getWidthHeightSelect(e.clientX, e.clientY)
 
 	markCanvas.style.top = `${top}px`
 	markCanvas.style.left = `${left}px`
@@ -124,3 +181,17 @@ document.addEventListener("pointermove", handleMouseMove);
 
 document.addEventListener("pointerup", handleMouseUp);
 
+lineArea.addEventListener("dragstart", (e) => {
+	if (!textItemData.length) return
+
+	const currentItem = e.target.closest('span')
+
+	const ItemData = textItemData.filter((item) => { return item.id === currentItem.id && item.isMarked })
+
+	if (!ItemData) {
+		console.log("no data drag")
+		return
+	}
+
+
+});
